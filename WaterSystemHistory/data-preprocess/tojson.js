3afcbe5d265bab73bde791577d67b6d058bf116e
carterm <carter.medlin@gmail.com>
2020-03-16 12:24:22 -0700

    Removed Water System History

--- WaterSystemHistory/data-preprocess/tojson.js
+++
@@ -1,9 +0,0 @@
-const csv=require('csvtojson')
-const fs = require('fs')
-
-csv()
-.fromFile('./analytes.csv')
-.then((jsonObj)=>{
-  console.log(jsonObj);
-  fs.writeFileSync('../analytes.json',JSON.stringify(jsonObj),'utf8')
-})
