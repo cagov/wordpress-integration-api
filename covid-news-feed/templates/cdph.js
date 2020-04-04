faff657e01092677cdd54eaf214346a15c3df905
carterm <carter.medlin@gmail.com>
2020-04-03 17:17:45 -0700

    hiding 11ty output using permalink: false

--- covid-news-feed/templates/cdph.js
+++ covid-news-feed/templates/cdph.js
@@ -7,6 +7,6 @@ module.exports = function createCDPHItem(sourceFile) {
   if(linkUrl.indexOf('http') === -1) {
     linkUrl = 'https://www.cdph.ca.gov'+sourceFile.url;
   }
-  newFile.html = `---\nlayout: "page.njk"\ntitle: "${sourceFile.title}"\nmeta: "${sourceFile.description}"\ntags: "guidancefeed"\nurl: "${linkUrl}"\nauthor: "California Department of Public Health"\npublishdate: "${sourceFile.date}"\n---\n\n<p><a href="${linkUrl}">${sourceFile.title}</a></p><p>${sourceFile.description}</p>`
+  newFile.html = `---\nlayout: "page.njk"\ntitle: "${sourceFile.title}"\nmeta: "${sourceFile.description}"\ntags: "guidancefeed"\nurl: "${linkUrl}"\nauthor: "California Department of Public Health"\npublishdate: "${sourceFile.date}"\npermalink: false\n---\n\n<p><a href="${linkUrl}">${sourceFile.title}</a></p><p>${sourceFile.description}</p>`
   return newFile;
 }
